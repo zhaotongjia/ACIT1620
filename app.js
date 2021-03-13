1b8ebfed1a5cda0408aa1161ed7f436cd763ce64
JOY JIA <jiazhaotongjoy@gmail.com>
2021-03-12 21:27:38 -0800

    add java

---
+++ app.js
@@ -0,0 +1,29 @@
+let num1 = 10
+
+let num2 = 5
+
+let first_sentence = 'Happy Spring Break'
+
+let first_boolean = true
+
+if (num1 === num2) {console.log('they match')} else {console.log('no luck')}
+VM2085:1 no luck
+
+for (let word in first_sentence) {console.log(first_sentence[word])}
+VM2103:1 H
+VM2103:1 a
+2VM2103:1 p
+VM2103:1 y
+VM2103:1  
+VM2103:1 S
+VM2103:1 p
+VM2103:1 r
+VM2103:1 i
+VM2103:1 n
+VM2103:1 g
+VM2103:1  
+VM2103:1 B
+VM2103:1 r
+VM2103:1 e
+VM2103:1 a
+VM2103:1 k
